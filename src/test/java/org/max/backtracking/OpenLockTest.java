7367d37b4e0624c7bc92ca6da731d5c9d0641b5d
Max <max.gomlya@gmail.com>
2024-08-16 00:27:15 +0800

    added backtracking open lock task

--- src/test/java/org/max/backtracking/OpenLockTest.java
+++ src/test/java/org/max/backtracking/OpenLockTest.java
@@ -1,6 +1,10 @@
 package org.max.backtracking;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
 You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
@@ -34,38 +38,39 @@ Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], tar
 Output: -1
 Explanation: We cannot reach the target without getting stuck.
  */
-public class OpenLock {
-    public static void main(String [] args) {
-        int res = open("0000", new String [] {"0201","0101","0102","1212","2002"}, "0202");
-        System.out.println(res);
+public class OpenLockTest {
 
-        res = open("0000", new String [] {"8888"}, "0009");
-        System.out.println(res);
+    @Test
+    void openLock() {
+        int res = open(new String[]{"0201", "0101", "0102", "1212", "2002"}, "0202");
+        assertEquals(6, res);
 
-        res = open("0000", new String [] {"8887","8889","8878","8898","8788","8988","7888","9888"}, "8888");
-        System.out.println(res);
+        res = open(new String[]{"8888"}, "0009");
+        assertEquals(1, res);
+
+        res = open(new String[]{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"}, "8888");
+        assertEquals(-1, res);
     }
 
-    private static int open(String start, String [] deadends, String target) {
-        if (start.equals(target)) {
+    private int open(String[] deadends, String target) {
+        if ("0000".equals(target)) {
             return 0;
         }
 
-        LinkedList<int []> queue = new LinkedList<>(); // keep the lock and steps count as last element [0, 0, 0, 0, 0]
-        Set<String> visited = new HashSet<>();
-        visited.addAll(Arrays.asList(deadends));
+        LinkedList<int[]> queue = new LinkedList<>(); // keep the lock and steps count as last element [0, 0, 0, 0, 0]
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
 
-        queue.offer(getLock(start, 0));
-        visited.add(start);
+        queue.offer(getLock("0000"));
+        visited.add("0000");
 
         while (!queue.isEmpty()) {
-            int [] lock = queue.poll();
+            int[] lock = queue.poll();
             if (target.equals(toString(lock))) {
                 return lock[4];
             }
 
-            List<int []> children = getChildren(lock);
-            for (int [] child : children) {
+            List<int[]> children = getChildren(lock);
+            for (int[] child : children) {
                 String strChild = toString(child);
                 if (!visited.contains(strChild)) {
                     visited.add(strChild);
@@ -77,15 +82,15 @@ public class OpenLock {
         return -1;
     }
 
-    private static List<int []> getChildren(int [] lock) {
-        List<int []> children = new ArrayList<>();
+    private List<int[]> getChildren(int[] lock) {
+        List<int[]> children = new ArrayList<>();
 
-        for (int i=0; i < lock.length - 1; i++) {
-            int [] newLock1 = new int [] {lock[0], lock[1], lock[2], lock[3], lock[4] + 1};
+        for (int i = 0; i < lock.length - 1; i++) {
+            int[] newLock1 = new int[]{lock[0], lock[1], lock[2], lock[3], lock[4] + 1};
             newLock1[i] = (newLock1[i] + 1) % 10;
             children.add(newLock1);
 
-            int [] newLock2 = new int [] {lock[0], lock[1], lock[2], lock[3], lock[4] + 1};
+            int[] newLock2 = new int[]{lock[0], lock[1], lock[2], lock[3], lock[4] + 1};
             newLock2[i] = (newLock2[i] - 1 + 10) % 10;
             children.add(newLock2);
         }
@@ -93,20 +98,16 @@ public class OpenLock {
         return children;
     }
 
-    private static int [] getLock(String lock, int steps) {
-        return new int [] {Character.getNumericValue(lock.charAt(0)), Character.getNumericValue(lock.charAt(1)),
-                Character.getNumericValue(lock.charAt(2)), Character.getNumericValue(lock.charAt(3)), steps};
+    private int[] getLock(String lock) {
+        return new int[]{Character.getNumericValue(lock.charAt(0)), Character.getNumericValue(lock.charAt(1)),
+                Character.getNumericValue(lock.charAt(2)), Character.getNumericValue(lock.charAt(3)), 0};
     }
 
-    private static String toString(int [] lock) {
-       StringBuilder res = new StringBuilder();
-
-       res.append(String.valueOf(lock[0]));
-       res.append(String.valueOf(lock[1]));
-       res.append(String.valueOf(lock[2]));
-       res.append(String.valueOf(lock[3]));
-
-       return res.toString();
+    private String toString(int[] lock) {
+        return String.valueOf(lock[0]) +
+                lock[1] +
+                lock[2] +
+                lock[3];
     }
 
 }
